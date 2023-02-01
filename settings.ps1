4fb0d1399502b909e3cf31745e6f39a04a69a214
Michael J. Stallinger <mj@stallinger.dev>
2023-02-01 02:02:07 +0100

    Add settings & config script

---
+++ settings.ps1
@@ -0,0 +1,11 @@
+function Get-RestoreToPath {
+    <#
+        .SYNOPSIS
+            Returns the location of the a path, the wsl distro should be restored to ().
+        .EXAMPLE
+            Get-Help -Name Test-Help
+
+            This shows the help for the example function.
+    #>
+    return 'test'
+}
