9c2ce51fba6e82aa3006225616cf4c7689ffda75
Michael J. Stallinger <mj@stallinger.dev>
2023-02-01 02:01:03 +0100

    Add file comment header and param check

--- wsl-backup.ps1
+++ wsl-backup.ps1
@@ -1,11 +1,32 @@
-# Creates a backup for the personal wsl instance (Ubuntu).
-# Requires wsl to be installed.
-param ($distro = 'Ubuntu')
-# export path (by convention) 
+<#
+    .SYNOPSIS
+        WSL backup script.
+    .DESCRIPTION
+        Creates a backup for the personal wsl instance (default: Ubuntu).
+        Requires wsl to be installed.
+    .EXAMPLE
+        PS> wsl-backup myUbuntu
+        Will backup the distro instance named 'myUbuntu'.
+#>
+param (
+    # Name of the distribution to be backed up.
+    [string] $distro
+)
+# check for default param (ToDo: remove redundancy)
+if (!$distro) {
+    $choice = Read-Host "Param '-distro' not provideed. Continue wirh default (Ubuntu)? [y/n]"
+    if ($choice -eq 'y') {
+        $distro = 'Ubuntu'
+    } else {
+        return
+    }
+}
+
+# export path (by convention)
 $exportPath = Join-Path -Path $([System.Environment]::GetFolderPath('MyDocuments')) -ChildPath 'wsl'
 # file name including date, host and distro
 $tarFileName = "$($(Get-Date).ToString('yyyy-MM-dd'))_$(hostname)_$($distro).tar"
-$exportFileName = Join-Path -Path $exportPath -ChildPath $tarFileName;
+$exportFileName = Join-Path -Path $exportPath -ChildPath $tarFileName
 # run export
 Write-Output "Exporting to '$exportFileName'..."
 wsl --export $distro $exportFileName
--- wsl-restore.ps1
+++ wsl-restore.ps1
@@ -1,14 +1,37 @@
-# Restores the most recent backup of the personal wsl instance (mjUbuntu) to $env:USERPROFILE ??
-param ($distro = 'mjUbuntu')
-# restore path (by convention) 
-$restorePath = Join-Path -Path $([System.Environment]::GetFolderPath('MyDocuments')) -ChildPath 'wsl'
-$importFileName = 'findLatestTARfileWithDistroNameInPath';
+<#
+    .SYNOPSIS
+        WSL restore script.
+    .DESCRIPTION
+        Restores the most recent backup of the personal wsl instance (default: Ubuntu) to $env:LOCALAPPDATA.
+        Requires wsl to be installed.
+    .EXAMPLE
+        PS> wsl-restore myUbuntu
+        Will restore the most recent backup file for distro instance named 'myUbuntu'.
+#>
+param (
+    # Name of the distribution to be restored.
+    [string] $distro
+)
+# check for default param (ToDo: remove redundancy)
+if (!$distro) {
+    $choice = Read-Host "Param '-distro' not provideed. Continue wirh default (Ubuntu)? [y/n]"
+    if ($choice -eq 'y') {
+        $distro = 'Ubuntu'
+    } else {
+        return
+    }
+}
+
+# restore path (by convention)
+# mkDir
+$restorePath = Join-Path -Path $([System.Environment]::GetFolderPath('MyDocuments')) -ChildPath "wsl + $distro"
+$importFileName = 'findLatestTARfileWithDistroNameInPath'
 
 # remove distro, if exists AND IF IMPORT-FILE FOUND!
 # wsl --unregister $distro
 
 # run import
-Write-Output "Importing from '$importFileName' to ..."
+Write-Output "Importing from '$importFileName' to $restorePath"
 
 # wsl --import Ubuntu c:\Users\mj\AppData\Local\wsl c:\Users\mj\OneDrive\Data\wsl\2023-01-29_Maja_Ubuntu.tar
 
